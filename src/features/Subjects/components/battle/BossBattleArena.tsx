173aca034d3b832a1c4cd92c1816f095dfb5cc01
Fhil Joshua <husphil@users.noreply.github.com>
2025-07-26 09:44:44 +0800

    also removed the corner decoration in the boss battle arena

--- src/features/Subjects/components/battle/BossBattleArena.tsx
+++ src/features/Subjects/components/battle/BossBattleArena.tsx
@@ -30,7 +30,6 @@ export const BossBattleArena = ({}: BossBattleArenaProps) => {
 		<div className={`flex flex-col w-[280px] items-center gap-4`}>
 			<div className="flex relative w-full h-[200px] overflow-hidden">
 				{/* needs to have div to be relative to */}
-				<CornerDecoration />
 				{/* Player (fixed on the left) */}
 				<SpriteSheet
 					style={{
