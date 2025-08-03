77700fcfee7c0f8aa50e236fbd7b5aae61f8ac19
Fhil Joshua <husphil@users.noreply.github.com>
2025-08-03 21:59:18 +0800

    added a setSkinUrl in the playerSlice of userPlayerStore to immidiately indicate that a player has equipped a certain skin

--- src/features/Auth/stores/userPlayerStore/profileSlice.ts
+++ src/features/Auth/stores/userPlayerStore/profileSlice.ts
@@ -3,13 +3,18 @@ import { StateCreator } from 'zustand';
 export interface ProfileSlice {
 	profileId: number | null;
 	avatarUrl: string | null;
+	skinUrl: string | null;
 	bio: string | null;
 	mood: string | null;
 
 	setProfile: (
 		payload: Omit<ProfileSlice, 'setProfile' | 'getProfile'>
 	) => void;
-	getProfile: () => Omit<ProfileSlice, 'setProfile' | 'getProfile'>;
+	setSkinUrl: (skinUrl: string) => void;
+	getProfile: () => Omit<
+		ProfileSlice,
+		'setProfile' | 'getProfile' | 'setSkinUrl'
+	>;
 }
 
 export const createProfileSlice: StateCreator<
@@ -20,16 +25,20 @@ export const createProfileSlice: StateCreator<
 > = (set, get) => ({
 	profileId: null,
 	avatarUrl: null,
+	skinUrl: null,
 	bio: null,
 	mood: null,
 
 	setProfile: (payload) => set({ ...payload }),
+
+	setSkinUrl: (skinUrl) => set({ skinUrl }),
 
 	getProfile: () => {
 		const s = get();
 		return {
 			profileId: s.profileId,
 			avatarUrl: s.avatarUrl,
+			skinUrl: s.skinUrl,
 			bio: s.bio,
 			mood: s.mood,
 		};
