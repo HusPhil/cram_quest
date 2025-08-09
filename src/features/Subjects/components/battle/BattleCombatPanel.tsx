290791abece42f1cc30adb162ff8865d6aa3b902
Fhil Joshua <husphil@users.noreply.github.com>
2025-08-09 20:16:07 +0800

    changed the action call button from 'attack'  to 'kill this task'

--- src/features/Subjects/components/battle/BattleCombatPanel.tsx
+++ src/features/Subjects/components/battle/BattleCombatPanel.tsx
@@ -69,7 +69,7 @@ export default function BattleCombatPanel({
 				}}
 			>
 				{/* <TbSword className="w-5 h-5 mr-2" color={colors.secondary} /> */}
-				ATTACK
+				KILL THIS TASK
 			</PixelButton>
 		</div>
 	);
