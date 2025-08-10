0e347bea589cda17526114386fcfe2a6638e0ed9
Fhil Joshua <husphil@users.noreply.github.com>
2025-08-10 19:41:23 +0800

    improve the ui of Learning Page as well as the cards itself

--- src/features/Subjects/components/learning/LearningMaterialCard.tsx
+++ src/features/Subjects/components/learning/LearningMaterialCard.tsx
@@ -9,6 +9,7 @@ import {
 import { MaterialType } from '../../screens/SubjectScreen/Tabs/LearningPage';
 import { MaterialRead } from '../../../../services/api/schema/material_schema';
 import { useSubjectStore_UI } from '../../stores/subjectStore_UI';
+import { TbBrandYoutube, TbCreditCard, TbNote } from 'react-icons/tb';
 
 interface LearningMaterialCardProps {
 	material: MaterialRead;
@@ -23,11 +24,11 @@ export default function LearningMaterialCard({
 	const getIconFromMaterialType = useCallback((type: MaterialType) => {
 		switch (type) {
 			case 'Note':
-				return <FaNoteSticky className="w-4 h-4" />;
+				return <TbNote className="w-7 h-7 text-background" />;
 			case 'Video':
-				return <FaCirclePlay className="w-4 h-4" />;
+				return <TbBrandYoutube className="w-7 h-7 text-background" />;
 			case 'Flashcard':
-				return <FaRug className="w-4 h-4" />;
+				return <TbCreditCard className="w-7 h-7 text-background" />;
 			default:
 				return null;
 		}
@@ -36,35 +37,35 @@ export default function LearningMaterialCard({
 	return (
 		<div
 			title={material.title}
-			className="flex items-center gap-2 py-3 justify-between mr-4"
+			onClick={() => {
+				window.open(material.link, '_blank');
+			}}
+			className="fade-in-on-view mx-2 flex items-center gap-2 py-3 justify-between bg-secondary border-white/10 border p-3 rounded-md lg:hover:opacity-50 transition-all duration-300 hover:cursor-pointer"
 		>
-			<div className="flex items-center gap-2">
-				<TagLabel className="rounded-full p-2 border shrink-0">
+			<div className="flex items-center gap-3">
+				<div className="rounded-md bg-accent p-2">
 					{getIconFromMaterialType(material.type as MaterialType)}
-				</TagLabel>
-				<div className="flex flex-col justify-center">
-					<a href={material.link} className="hover:text-accent">
-						<p className="line-clamp-1 text-sm">{material.title}</p>
-					</a>
-					<small className="text-text/40 text-xs">{`${material.type} • <TBI>`}</small>
+				</div>
+				<div className="flex flex-col gap-1 justify-center">
+					<p className="line-clamp-1 text-sm">{material.title}</p>
+					<small className="text-text/40 text-xs">{`${material.type} `}</small>
 				</div>
 			</div>
-			<span className="flex items-center justify-center h-full">
-				<button>
-					<FaEllipsisVertical
-						className="w-4 h-4"
-						onClick={() =>
-							setActiveModal('EditMaterialModal', {
-								id: material.id,
-								link: material.link,
-								subject_id: material.subject_id,
-								title: material.title,
-								type: material.type,
-							})
-						}
-					></FaEllipsisVertical>
-				</button>
-			</span>
+			<button
+				className="flex items-center justify-center h-full p-2 hover:bg-accent/25 rounded-md"
+				onClick={(event) => {
+					event.stopPropagation();
+					setActiveModal('EditMaterialModal', {
+						id: material.id,
+						link: material.link,
+						subject_id: material.subject_id,
+						title: material.title,
+						type: material.type,
+					});
+				}}
+			>
+				<FaEllipsisVertical className="w-5 h-5" />
+			</button>
 		</div>
 	);
 }
--- src/features/Subjects/components/learning/LearningMaterials.tsx
+++ src/features/Subjects/components/learning/LearningMaterials.tsx
@@ -13,28 +13,8 @@ export default function SubjectMaterials({
 	subjectId,
 	materials,
 }: LearningMaterialsProps) {
-	const setActiveModal = useSubjectStore_UI((state) => state.setActiveModal);
-
 	return (
 		<>
-			{/* Header */}
-			<div className="flex items-center justify-between my-3">
-				<span className="flex gap-2">
-					<h1 className="text-md font-bold">Materials</h1>
-					<TagLabel className="flex items-center rounded-full px-2">
-						<p className="text-xs">{materials.length.toString()}</p>
-					</TagLabel>
-				</span>
-				<span className="flex text-sm items-center bg-accent text-white p-1 rounded-md px-3">
-					<button
-						className="flex items-center gap-1"
-						onClick={() => setActiveModal('AddNewMaterialModal')}
-					>
-						Add <FaPlus className="w-3 h-3" />
-					</button>
-				</span>
-			</div>
-
 			{/* Materials */}
 			{materials.map((material, index) => (
 				<LearningMaterialCard
