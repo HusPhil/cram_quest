cf85132fd3e12050e77e931fbb763f97c0a5c8e5
Fhil Joshua <husphil@users.noreply.github.com>
2025-08-15 20:43:34 +0800

    added redirect on first visit to about

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import FloatingScreen from '../components/FloatingScreen.tsx/FloatingScreen';
+import { useEffect } from 'react';
 
 const Home = () => {
+	const navigate = useNavigate();
+	useEffect(() => {
+		const hasVisited = localStorage.getItem('hasVisited');
+		if (!hasVisited) {
+			localStorage.setItem('hasVisited', 'true');
+			navigate('/home/about', { replace: true });
+		}
+	}, [navigate]);
 	return (
 		<div className="h-full w-full flex flex-col relative items-center">
 			{/* Main Display Area */}
