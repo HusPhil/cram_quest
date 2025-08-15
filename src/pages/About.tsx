47cbaa65b84dd6d839339ae54003a5bbc83cd25f
Fhil Joshua <husphil@users.noreply.github.com>
2025-08-15 21:07:29 +0800

    added a button to redirect to check-in in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,15 +1,19 @@
+import { TbBackpack } from 'react-icons/tb';
+import { useNavigate } from 'react-router-dom';
+
 export default function About() {
+	const navigate = useNavigate();
 	return (
 		<div className="flex w-full h-full flex-col items-center justify-center bg-background px-5 py-12 text-gray-300">
-			<div className="w-full max-w-4xl rounded-lg border border-white/10 bg-secondary py-5 px-8 shadow-lg md:p-12 ">
-				<h1 className="mb-3 text-center font-rpg text-4xl font-bold text-accent">
+			<div className="w-full max-w-4xl max-h-[70dvh] rounded-lg border border-white/10 bg-secondary py-5 px-8 shadow-lg md:p-12 ">
+				<h1 className="mb-3 text-center font-rpg text-2xl md:text-3xl font-bold text-accent">
 					Welcome to CramQuest!
 				</h1>
-				<p className="mb-6 text-center text-lg leading-relaxed">
-					A simple app for helping you Gamify your Tasks!
+				<p className="md:mb-2 text-center leading-relaxed break-words">
+					{"< Let's Gamify your Tasks >"}
 				</p>{' '}
 				<br />
-				<p className="mb-6 text-justify text-lg leading-relaxed">
+				<p className="mb-3 leading-relaxed text-sm md:text-base">
 					<span className="text-center text-accent">
 						Please don't hesitate to reach out with any thoughts or
 						bug reports!{' '}
@@ -18,7 +22,7 @@ export default function About() {
 					important to us as we work to add more features and refine
 					the gameplay.
 				</p>
-				<p>
+				<p className="mb-6 leading-relaxed text-sm md:text-base">
 					Connect with us at{' '}
 					<a
 						href="mailto:mymail.com"
@@ -45,6 +49,15 @@ export default function About() {
 					</p>
 				</div>
 			</div>
+			<button
+				type="button"
+				className="flex items-center justify-center py-2 gap-3 px-5 bg-accent rounded-md mt-7 text-background animate-bounce"
+				onClick={() => navigate('/home/check-in')}
+			>
+				<TbBackpack className="w-5 h-5" />
+				<p>Start your Journey</p>
+				<TbBackpack className="w-5 h-5" />
+			</button>
 		</div>
 	);
 }
